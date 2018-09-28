b42582094337fb63778a107e9f7410f60e221267
cheungngo <cheungngo@gmail.com>
2018-09-29 01:53:29 +0800

    edit

--- 20180929 Testing.R
+++ 20180929 Testing.R
@@ -1,3 +1,10 @@
 # Introduction to Git and GitHub  
 # RStudio, Git, and GitHub
 
+# First to save the file
+
+# Second 'add', to check the 'staged' box on the right under the Git menu
+
+# Third 'commit', just click on the commit button
+
+# Fourth 'push', just click the push button
